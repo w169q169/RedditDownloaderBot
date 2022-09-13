61df82cef3f3d2cb3f97700997a61b13f05b2e6c
senhtry <w169q169@gmail.com>
2022-09-13 13:44:36 +0800

    update video link

--- reddit/utils.go
+++ reddit/utils.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
+	"log"
 )
 
 func GetRedgifsVideo(body io.Reader) (videoUrl string, err error) {
@@ -11,16 +12,21 @@ func GetRedgifsVideo(body io.Reader) (videoUrl string, err error) {
 	if err != nil {
 		return "", err
 	}
+	urlList := []string{}
+
 	doc.Find("meta").Each(func(i int, s *goquery.Selection) {
 		if name, _ := s.Attr("property"); name == "og:video" {
-
 			videoUrl, _ = s.Attr("content")
+			log.Println("get video url:", videoUrl)
+			urlList = append(urlList, videoUrl)
 		}
 	})
 
-	if videoUrl == "" {
+	if len(urlList) == 0 {
 		return "", fmt.Errorf("cant find meta")
 	}
+
+	videoUrl = urlList[0]
 
 	return videoUrl, nil
 }
